86709db735c73a05c92891df677fbee3ad3eca99
FrancysNavas <francys.navas.sanchez@gmail.com>
2023-01-09 15:09:50 -0300

    SE creo el Banner

--- portfolio-FrontEnd/src/app/app.module.ts
+++ portfolio-FrontEnd/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
