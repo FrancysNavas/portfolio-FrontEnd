b85b1174e07e0fd439d019ec59d3b199ac966d40
FrancysNavas <francys.navas.sanchez@gmail.com>
2023-01-09 21:17:39 -0300

    Se creo el area de EDUCACION y SKILLS

--- portfolio-FrontEnd/src/app/app.module.ts
+++ portfolio-FrontEnd/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
     NavbarComponent,
     BannerComponent,
     AcercaDeComponent,
-    ExperienciaComponent
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
