01fc0349ecb7e37c93f8f0334ecf9605be7f7ab1
FrancysNavas <francys.navas.sanchez@gmail.com>
2023-01-27 10:18:12 -0300

    Configuracion Routing

--- portfolio-FrontEnd/src/app/app-routing.module.ts
+++ portfolio-FrontEnd/src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NeweducacionComponent } from './components/educacion/neweducacion.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { NewEducacionComponent } from './components/educacion/new-educacion.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'login', component: LoginComponent},
   { path: 'nuevaexp', component: NewExperienciaComponent},
   { path: 'editexpe/:id', component: EditExperienciaComponent },
-  { path: 'nuevaedu', component: NeweducacionComponent }
+  { path: 'nuevaedu', component: NewEducacionComponent }
 ];
 
 @NgModule({
